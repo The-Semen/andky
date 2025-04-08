f3aa830882e90e3dfaeb79482ef74b65439992ec
The-Semen <sudavcovsemen@gmail.com>
2025-04-08 18:48:47 +0300

    Add information about local repository in readme file

---
+++ pract4_1.py
@@ -0,0 +1,4 @@
+tanaAge = 14
+mihaAge = 10
+
+print("Средний возраст Тани и Миши", int((tanaAge+mihaAge)/2), "лет")
---
+++ pract4_2.py
@@ -0,0 +1,9 @@
+import random as rd
+
+firstNum = rd.randint(1,8)
+secondNum = rd.randint(0,9)
+thrithNum = rd.randint(0,9)
+
+print(firstNum*10 + secondNum)
+print(thrithNum)
+print(firstNum*10 + secondNum + thrithNum)
